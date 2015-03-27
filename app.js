3f3f2bea7f53a01aafad43da152e0960b6defe5e
lanshayu <fingnet@gmail.com>
2015-03-28 02:05:52 +0800

    clean

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-app.use('/', express.static('./static'));
-
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
-});
